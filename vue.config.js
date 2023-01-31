f8f300c1879971719133dcf8602dc863154ccdee
lizhigang <d_iii@aliyun.com>
2023-01-31 17:44:49 +0800

    feat: 代码重新上传
    由于上一版该分支残留一些旧分支的文件导致构建与调试报错，在本次提交中修复。

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+// const registerRouter = require('./backend/router')
+const router = require('./backend/router')
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和 mixin
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  },
+  devServer: {
+    before(app) {
+      app.use('/', router)
+    }
+  },
+  configureWebpack: (config) => {
+    if (process.env.npm_config_report) {
+      const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+      config.plugins.push(new BundleAnalyzerPlugin())
+    }
+  },
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/music/' : '/'
+}
